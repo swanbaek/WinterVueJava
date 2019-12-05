21ee1fda5837e6dbabf421851328619c5cd649e7
swan <swanbaek@naver.com>
2019-12-05 17:10:19 +0900

    컴포넌트 분리하여 기본 홈페이지 만들기
    뷰라우터와 httpVueLoader 활용

--- MyVueJava/WebContent/js/app.js
+++ MyVueJava/WebContent/js/app.js
@@ -4,6 +4,7 @@
 $(function(){
 	var app=new Vue({
 		el:'#app',
+		router:router,
 		components:{
 			'top-nav':httpVueLoader('components/common/Top_nav.vue'),
 			'foot-comp':httpVueLoader('components/common/Footer.vue')
